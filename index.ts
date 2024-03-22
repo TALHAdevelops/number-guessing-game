64fb1b841961676ce93ac778157de38fcf6b67da
TALHAdevelops <160895671+TALHAdevelops@users.noreply.github.com>
2024-03-23 00:27:43 +0500

    new change

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber=Math.floor(Math.random()*5);
+const answer= await inquirer.prompt([
+    {
+        name:"guess",
+        type:"number",
+        message:"Please Guess a number from 0 to 5",
+    }
+]);
+console.log(answer);
+if(answer.guess===randomNumber){
+    console.log("You won");
+}else{
+    console.log("You lost");
+}
+console.log(`The number is : ${randomNumber}`  );
+
